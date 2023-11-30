c09a0287f54b16ca870ba8a098c646627b75797f
Srownel <william@devouard.fr>
2023-11-30 19:25:28 +0100

    Update and rename default-template.js to default.js

--- config/default-template.js
+++
@@ -1,8 +0,0 @@
-const config = {
-  DISCORD_TOKEN: 'DISCORD_TOKEN',
-  "clientId": "CLIENT_ID",
-	"serv1Id": "SERV1_ID",
-  "serv2Id": "SERV2_ID"
-};
-
-module.exports = config;
---
+++ config/default.js
@@ -0,0 +1,6 @@
+const config = {
+  DISCORD_TOKEN: process.env.TOKEN,
+  "clientId": process.env.CLIENT_ID,
+};
+
+module.exports = config;
